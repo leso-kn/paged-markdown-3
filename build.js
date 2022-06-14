78cd64931cb5a742cbd6beb3355b3ce1fa612970
leso-kn <info@lesosoftware.com>
2022-06-14 15:46:44 +0200

    Build system: Added sass support

--- build.js
+++ build.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
 
+let sass = require('sass');
+
 tasks
 ({
     // TypeScript
@@ -11,5 +13,20 @@ tasks
         log('tsc', dest);
 
         execSync(__dirname + '/node_modules/.bin/spack');
-    }]
+    }],
+
+    // SCSS
+    'src/(**).scss': [ 'dist/$1.css', async (src, dest) =>
+    {
+        log('scss', dest);
+
+        fs.writeFileSync(dest,
+            sass.renderSync(
+            {
+                file: src,
+                outFile: dest,
+                sourceMap: debug,
+                sourceMapEmbed: true
+            }).css);
+    }],
 });
